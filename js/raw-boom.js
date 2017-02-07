94ef4bf41c21d3f0b7c0e4f315618f38176367f8
Zak Spence <zakspence@gmail.com>
2017-02-07 15:57:50 -0600

    first commit

---
+++ js/raw-boom.js
@@ -0,0 +1,171 @@
+"use strict";
+
+var rawExplosives = `
+A
+A-IX-2
+Amatex
+Amatol
+Ammonal
+Ammonium nitrate disasters
+Ammunition
+Ammunition categories for carriage on scheduled flights
+ANFO
+Argon flash
+Armstrong's mixture
+Astrolite
+B
+Ballistite
+Baratol
+Beyond-armour effect
+Boiling liquid expanding vapor explosion
+Brisance
+Brown powder
+C
+C-4 (explosive)
+Carbonite (explosive)
+Cheddite
+Chemical explosive
+Exploding cigar
+Composition B
+Composition C
+Composition H6
+Contact explosive
+Cordtex
+Cyclotol
+D
+Danubit
+Deflagration
+Dense Inert Metal Explosive
+Detasheet
+Detonating cord
+Detonator
+Detonator (railway)
+Drilling and blasting
+Dry Creek explosives depot
+Dualin
+Dunnite
+Dynamite
+Dyno Nobel
+E
+Ecrasite
+Ednatol
+Explosive booster
+Explosive Substances Act 1883
+Explosively formed penetrator
+Explosives engineering
+Explosives safety
+Explosives trace detector
+Explosophore
+F
+Flash powder
+G
+G2ZT
+Gelignite
+Goma-2
+Gunpowder
+Gunpowder magazine
+H
+High Blast Explosive
+Hexanite
+Hi-Fi Digimonster
+I
+Ice blasting
+Improved Military Rifle
+Improvised explosive device
+IMX-101
+J
+Jack’s Magazine
+List of Japanese World War II explosives
+K
+Kinetite
+L
+LX-14
+M
+Irvine Masson
+Mexal 1500
+Minol (explosive)
+Misznay–Schardin effect
+Munroe effect
+N
+Nano-thermite
+Net explosive quantity
+NFPA 1123
+Nobel 808
+Nonel
+North Arm Powder Magazine
+O
+Octol
+OKFOL
+Overpressure
+Oxyliquit
+P
+Panclastite
+PE4
+Pentolite
+Petroleum and Explosives Safety Organisation
+Phlegmatized
+Piccolo (firecracker)
+Picratol
+Plastic explosive
+Plastic Igniter Cord
+PNNM
+Poudre B
+Pressure cooker bomb
+Primacord
+Primary and secondary explosives
+Primasheet
+Pyrotechnics
+Pyrotol
+R
+Reactive material
+S
+Safety fuse
+Safety testing of explosives
+Saucisson (pyrotechnics)
+Semtex
+Sensitivity (explosives)
+Sheet explosive
+Shellite (explosive)
+Shimose powder
+Shock factor
+Shock sensitivity
+Smokeless powder
+Sprengel explosive
+Squib (explosive)
+Steam explosion
+Strength (explosive)
+Surgically implanted explosive device
+T
+Tannerite
+Thermalite
+Titadine
+TNT equivalent
+Tonite (explosive)
+Torpex
+Tovex
+Explosive train
+Tritonal
+W
+Water gel explosive
+Webster's reagent
+Z
+ZND detonation model
+`;
+
+var explodingObj = '';
+
+//get the position of the first newLine and the second newline
+//substring() assigns the substring to a variable the string
+//test the string to see if it is one character long
+//if so,reject the string
+//else add the word and its properties to a JSON file with properties 'id' and 'price'
+
+var sortedExplosives = (explosiveList) => {
+	var explosiveList = explosiveList.split(/\n/ || /\b/);
+	var listLength = explosiveList.lastIndexOf(/\n/);
+	for(var i = 0; i < length; i++) {
+		if ()
+	}
+}();
+
+module.exports = explodingObj;
---
+++ raw-boom.js
@@ -0,0 +1,166 @@
+"use strict";
+
+var rawExplosives = `
+A
+A-IX-2
+Amatex
+Amatol
+Ammonal
+Ammonium nitrate disasters
+Ammunition
+Ammunition categories for carriage on scheduled flights
+ANFO
+Argon flash
+Armstrong's mixture
+Astrolite
+B
+Ballistite
+Baratol
+Beyond-armour effect
+Boiling liquid expanding vapor explosion
+Brisance
+Brown powder
+C
+C-4 (explosive)
+Carbonite (explosive)
+Cheddite
+Chemical explosive
+Exploding cigar
+Composition B
+Composition C
+Composition H6
+Contact explosive
+Cordtex
+Cyclotol
+D
+Danubit
+Deflagration
+Dense Inert Metal Explosive
+Detasheet
+Detonating cord
+Detonator
+Detonator (railway)
+Drilling and blasting
+Dry Creek explosives depot
+Dualin
+Dunnite
+Dynamite
+Dyno Nobel
+E
+Ecrasite
+Ednatol
+Explosive booster
+Explosive Substances Act 1883
+Explosively formed penetrator
+Explosives engineering
+Explosives safety
+Explosives trace detector
+Explosophore
+F
+Flash powder
+G
+G2ZT
+Gelignite
+Goma-2
+Gunpowder
+Gunpowder magazine
+H
+High Blast Explosive
+Hexanite
+Hi-Fi Digimonster
+I
+Ice blasting
+Improved Military Rifle
+Improvised explosive device
+IMX-101
+J
+Jack’s Magazine
+List of Japanese World War II explosives
+K
+Kinetite
+L
+LX-14
+M
+Irvine Masson
+Mexal 1500
+Minol (explosive)
+Misznay–Schardin effect
+Munroe effect
+N
+Nano-thermite
+Net explosive quantity
+NFPA 1123
+Nobel 808
+Nonel
+North Arm Powder Magazine
+O
+Octol
+OKFOL
+Overpressure
+Oxyliquit
+P
+Panclastite
+PE4
+Pentolite
+Petroleum and Explosives Safety Organisation
+Phlegmatized
+Piccolo (firecracker)
+Picratol
+Plastic explosive
+Plastic Igniter Cord
+PNNM
+Poudre B
+Pressure cooker bomb
+Primacord
+Primary and secondary explosives
+Primasheet
+Pyrotechnics
+Pyrotol
+R
+Reactive material
+S
+Safety fuse
+Safety testing of explosives
+Saucisson (pyrotechnics)
+Semtex
+Sensitivity (explosives)
+Sheet explosive
+Shellite (explosive)
+Shimose powder
+Shock factor
+Shock sensitivity
+Smokeless powder
+Sprengel explosive
+Squib (explosive)
+Steam explosion
+Strength (explosive)
+Surgically implanted explosive device
+T
+Tannerite
+Thermalite
+Titadine
+TNT equivalent
+Tonite (explosive)
+Torpex
+Tovex
+Explosive train
+Tritonal
+W
+Water gel explosive
+Webster's reagent
+Z
+ZND detonation model
+`;
+
+"use strict";
+
+var sortedExplosives = (rawExplosives) => {
+	debugger;
+	var newLines = rawExplosives.match(/\n/g);
+	var length =
+	for(var i = 0; i < rawExplosives.inde; i++) {
+
+	}
+}();
+
+module.exports = explodingObj;
